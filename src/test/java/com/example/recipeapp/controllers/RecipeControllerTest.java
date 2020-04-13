45e0dbb44e226fc3db03d6682ec3609fc56b3ba9
labtec96 <cezaryhan1@gmail.com>
2020-04-13 18:35:17 +0200

    delete unnecessary test

--- src/test/java/com/example/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/example/recipeapp/controllers/RecipeControllerTest.java
@@ -18,30 +18,4 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class RecipeControllerTest {
 
-
-        @Mock
-        RecipeService recipeService;
-
-        RecipeController controller;
-
-        @BeforeEach
-        void setUp() throws Exception {
-            MockitoAnnotations.initMocks(this);
-
-            controller = new RecipeController(recipeService);
-        }
-
-        @Test
-        void testGetRecipe() throws Exception {
-
-            Recipe recipe = new Recipe();
-            recipe.setId(1L);
-
-            MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-
-            when(recipeService.findById(anyLong())).thenReturn(recipe);
-
-            mockMvc.perform(get("/recipe/show/1"))
-                    .andExpect(status().isOk());
-        }
 }
